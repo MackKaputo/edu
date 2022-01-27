b28857e112bf315aa218c30a17431bea0144bdf0
MackKaputo <mackkaputo@gmail.com>
2022-01-28 01:51:18 +0200

    styled initial navbar

--- src/components/GlobalNav/GlobalNav.jsx
+++ src/components/GlobalNav/GlobalNav.jsx
@@ -1,26 +1,40 @@
 import React, { useContext } from 'react'
 import { Link } from "react-router-dom"
 import { UserContext } from '../../context'
+import styles from "./GlobalNav.module.css"
 
 function GlobalNav() {
   const [user, setUser] = useContext(UserContext)
 
   return (
-    <nav>
-        <div><Link to="/">MyLogoMyLogo</Link></div>
-        <div>
-            <ul>
-                <li>About</li>
-                <li>
-                    <Link to="/courses">Courses</Link>
-                </li>
-                <li><Link to="/mycourses" >My Courses</Link></li>
-                <li>Blog</li>
-            </ul>
-        </div>
-        <div>
-          {user.name}
-        </div>
+    <nav className={styles.global_nav}>
+        <ul>
+            <li><Link to="/">TechEqual</Link></li>
+        </ul>
+
+        <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/courses">Courses</Link>
+            </li>
+            <li>
+              <Link to="/courses">Blog</Link>
+            </li>
+            <li>
+              <Link to="/mycourses" >MyCourses</Link>
+            </li>
+            <li>
+              <Link to="/">{!user.name? "Login" : "Logout"}</Link>
+            </li>
+        </ul>
+
+        <ul>
+          <li>
+            <Link to="/courses">{user.name}</Link>
+          </li>
+        </ul>
     </nav>
   )
 }
